aeae3df411650498c06a4dcce0e7b9e10b13e896
Ludo <ludovico.ferrara@ruhr-uni-bochum.de>
2021-08-04 08:50:09 +0200

    update

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 from kivymd.app import MDApp
+import sqlite3
 from topspotmap import TopSpotMap
 class MainApp(MDApp):
-    pass
+    connection = None
+    cursor = None
+    def on_start(self):
+        #init gps
+
+        #init database
+        self.connection = sqlite3.connect("spotlist.db")
+        self.cursor = self.connection.cursor()
+        #init SearchPopupMenu
 
 if __name__ == "__main__":
     MainApp().run()
--- topspotmap.py
+++ topspotmap.py
@@ -1,10 +1,12 @@
 from kivy_garden.mapview import MapView
 from kivy.clock import Clock
+from kivy.app import App
 
 class TopSpotMap(MapView):
     getting_spots_timer = None
+
     def start_getting_spots_in_fov(self):
-        #load spots after 1 sec
+        # After one second, get the spots in the field of view
         try:
             self.getting_spots_timer.cancel()
         except:
@@ -13,4 +15,16 @@ class TopSpotMap(MapView):
         self.getting_spots_timer = Clock.schedule_once(self.get_spots_in_fov, 1)
 
     def get_spots_in_fov(self, *args):
-        print(self.get_bbox())
+        min_lat, min_lon, max_lat, max_lon = self.get_bbox()
+        app = App.get_running_app()
+        sql_statement = "SELECT * FROM markets WHERE x > %s AND x < %s AND y > %s AND y < %s "%(min_lon, max_lon, min_lat, max_lat)
+        app.cursor.execute(sql_statement)
+        spots = app.cursor.fetchall()
+        print(spots)
+        for spot in spots:
+            self.add_spot(spot)
+
+    def add_spot(self, spot):
+        pass
+
+
