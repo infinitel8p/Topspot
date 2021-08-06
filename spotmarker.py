a6db7c19b2a904dd314b1eef8297ea14fd66b3f4
Ludo <ludovico.ferrara@ruhr-uni-bochum.de>
2021-08-06 06:36:39 +0200

    update

--- spotmarker.py
+++ spotmarker.py
@@ -1,7 +1,7 @@
 from kivy_garden.mapview import MapMarkerPopup, MapMarker
 
-class SpotMarker(MapMarker):
-    #source = "images/map_marker.png"
+class SpotMarker(MapMarkerPopup):
+    source = "images/map_marker.png"
     def on_release(self):
         pass
 
