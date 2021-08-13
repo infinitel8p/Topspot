f31e264c5332de09b810501041e944574f4b420a
Ludo <ludovico.ferrara@ruhr-uni-bochum.de>
2021-08-13 12:17:34 +0200

    update

--- test.py
+++ test.py
@@ -1,10 +1,3 @@
-headers = ['id', 'SpotName', 'x', 'y', 'MapsLink', 'Website', 'Street', 'City', 'Country', 'PLZ', 'Season1Date', 'Season1Time', 'Season2Date', 'Season2Time', 'Image1', 'Image2', 'Image3', 'Information', 'updateTime']
-
-for i in range(len(headers)):
-    attribute_name = headers[i]
-    attribute_value = str(spot_data[i])
-    setattr(attribute_name, attribute_value)
-print(id)
-print(x)
-print(y)
-print(SpotName)
+a= "a"
+b= "b"
+print(f"test{a,b}")
