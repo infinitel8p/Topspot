be9a6a7de41e8e7ec4e9392b2ef52f8d86b8e283
Ludo <ludovico.ferrara@ruhr-uni-bochum.de>
2021-08-13 12:01:41 +0200

    update

--- spotpopmenu.py
+++ spotpopmenu.py
@@ -6,42 +6,43 @@ class LocationPopupMenu(ListMDDialog):
     def __init__(self, spot_data):
         super().__init__()
 
-        headers = ['ID', 'SpotName', 'x', 'y', 'MapsLink', 'Website', 'Street', 'City', 'Country', 'PLZ', 'Season1Date', 'Season1Time', 'Season2Date', 'Season2Time', 'Image1', 'Image2', 'Image3', 'Information', 'updateTime']
+        headers = ['id', 'SpotName', 'x', 'y', 'MapsLink', 'Website', 'Street', 'City', 'Country', 'PLZ', 'Season1Date', 'Season1Time', 'Season2Date', 'Season2Time', 'Image1', 'Image2', 'Image3', 'Information', 'updateTime']
 
         for i in range(len(headers)):
             attribute_name = headers[i]
             attribute_value = str(spot_data[i])
             setattr(self, attribute_name, attribute_value)
-        print(self.Image1)
-        print(self.Image2)
-        print(self.Image3)
+        print(self.id)
+        print(self.x)
+        print(self.y)
+        print(self.SpotName)
         try:
             response = requests.get(str(self.Image1), verify=certifi.where())
-            file = open(f"image1{self.SpotName}.png", "wb")
+            file = open(f"image1{self.SpotName}{self.id}.png", "wb")
             file.write(response.content)
             file.close()
         except:
             response = requests.get("https://raw.githubusercontent.com/infinitel8p/Topspot/master/Images/missing.jpg", verify=certifi.where())
-            file = open(f"image1{self.SpotName}.png", "wb")
+            file = open(f"image1{self.SpotName}{self.id}.png", "wb")
             file.write(response.content)
             file.close()
         try:
             response = requests.get(str(self.Image2), verify=certifi.where())
-            file = open(f"image2{self.SpotName}.png", "wb")
+            file = open(f"image2{self.SpotName}{self.id}.png", "wb")
             file.write(response.content)
             file.close()
         except:
             response = requests.get("https://raw.githubusercontent.com/infinitel8p/Topspot/master/Images/missing.jpg", verify=certifi.where())
-            file = open(f"image2{self.SpotName}.png", "wb")
+            file = open(f"image2{self.SpotName}{self.id}.png", "wb")
             file.write(response.content)
             file.close()
         try:
             response = requests.get(str(self.Image3), verify=certifi.where())
-            file = open(f"image3{self.SpotName}.png", "wb")
+            file = open(f"image3{self.SpotName}{self.id}.png", "wb")
             file.write(response.content)
             file.close()
         except:
             response = requests.get("https://raw.githubusercontent.com/infinitel8p/Topspot/master/Images/missing.jpg", verify=certifi.where())
-            file = open(f"image3{self.SpotName}.png", "wb")
+            file = open(f"image3{self.SpotName}{self.id}.png", "wb")
             file.write(response.content)
             file.close()
