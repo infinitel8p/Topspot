751b1c11e229ee07f2d2b96716cfab43d6d5ed2e
Ludo <ludovico.ferrara@ruhr-uni-bochum.de>
2021-08-15 23:36:24 +0200

    update

--- GeoData/convert/convert.py
+++ GeoData/convert/convert.py
@@ -1,5 +1,5 @@
 with open("files.txt", "r") as data:
-    output = open("output.txt", "a")
+    output = open("coordinaters.txt", "a")
     for line in data:
-        line = (line.split(("\t")))
+        line = (line.split((" ")))
         output.write(f"https://www.google.com/maps/dir//{str(line)}\n")
