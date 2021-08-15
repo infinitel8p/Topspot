93a85d965fa09bd1279629229034f88f52525097
Ludo <ludovico.ferrara@ruhr-uni-bochum.de>
2021-08-15 21:47:25 +0200

    update

--- tests/old_dialog.py
+++ tests/old_dialog.py
@@ -1,76 +1,58 @@
 """
 Dialog
 ======
-
 Copyright (c) 2015 Andrés Rodríguez and KivyMD contributors -
     KivyMD library up to version 0.1.2
 Copyright (c) 2019 Ivanov Yuri and KivyMD contributors -
     KivyMD library version 0.1.3 and higher
-
 For suggestions and questions:
 <kivydevelopment@gmail.com>
-
 This file is distributed under the terms of the same license,
 as the Kivy framework.
-
 `Material Design spec, Dialogs <https://material.io/design/components/dialogs.html>`_
-
 Example
 -------
-
 from kivymd.app import MDApp
 from kivy.lang import Builder
 from kivy.factory import Factory
 from kivy.utils import get_hex_from_color
-
 from kivymd.uix.dialog import MDInputDialog, MDDialog
 from kivymd.theming import ThemeManager
-
-
 Builder.load_string('''
 <ExampleDialogs@BoxLayout>
     orientation: 'vertical'
     spacing: dp(5)
-
     MDToolbar:
         id: toolbar
         title: app.title
         left_action_items: [['menu', lambda x: None]]
         elevation: 10
         md_bg_color: app.theme_cls.primary_color
-
     FloatLayout:
         MDRectangleFlatButton:
             text: "Open input dialog"
             pos_hint: {'center_x': .5, 'center_y': .7}
             opposite_colors: True
             on_release: app.show_example_input_dialog()
-
         MDRectangleFlatButton:
             text: "Open Ok Cancel dialog"
             pos_hint: {'center_x': .5, 'center_y': .5}
             opposite_colors: True
             on_release: app.show_example_okcancel_dialog()
 ''')
-
-
 class Example(MDApp):
     title = "Dialogs"
-
     def build(self):
         return Factory.ExampleDialogs()
-
     def callback_for_menu_items(self, *args):
         from kivymd.toast.kivytoast import toast
         toast(args[0])
-
     def show_example_input_dialog(self):
         dialog = MDInputDialog(
             title='Title', hint_text='Hint text', size_hint=(.8, .4),
             text_button_ok='Yes',
             events_callback=self.callback_for_menu_items)
         dialog.open()
-
     def show_example_okcancel_dialog(self):
         dialog = MDDialog(
             title='Title', size_hint=(.8, .3), text_button_ok='Yes',
@@ -79,8 +61,6 @@ class Example(MDApp):
             text_button_cancel='Cancel',
             events_callback=self.callback_for_menu_items)
         dialog.open()
-
-
 Example().run()
 """
 
@@ -103,55 +83,43 @@ from kivymd.material_resources import DEVICE_IOS
 Builder.load_string(
     """
 #:import images_path kivymd.images_path
-
-
 <ContentInputDialog>
     orientation: 'vertical'
     padding: dp(15)
     spacing: dp(10)
-
     MDLabel:
         font_style: 'H6'
         text: root.title
         halign: 'left' if not root.device_ios else 'center'
-
     BoxLayout:
         id: box_input
         size_hint: 1, None
-
     Widget:
     Widget:
-
     MDSeparator:
         id: sep
-
     BoxLayout:
         id: box_buttons
         size_hint_y: None
         height: dp(20)
         padding: dp(20), 0, dp(20), 0
-
 #:import webbrowser webbrowser
 #:import parse urllib.parse
 <ThinLabel@MDLabel>:
     size_hint: 1, None
     valign: 'middle'
     height: self.texture_size[1]
-
 <ThinLabelButton@ThinLabel+MDTextButton>:
     size_hint_y: None
     valign: 'middle'
     height: self.texture_size[1]
-
 <ThinBox@BoxLayout>:
     size_hint_y: None
     height: self.minimum_height
     padding: dp(0), dp(0), dp(10), dp(0)
-
 <ThinImageBox@MDCard>:
     width: self.width
     height: self.height
-
 <ListMDDialog>
     title: ""
     BoxLayout:
@@ -167,34 +135,36 @@ Builder.load_string(
             size_hint_y: None
             text_size: self.width, None
             height: self.texture_size[1]
-
         ScrollView:
-            # scroll_timeout: 500
-            do_scroll_x: False
-            always_overscroll: True
-            scroll_type: ['bars', 'content']
-
+            id: scroll
+            height:
+                root.height - (title.height + dp(48)\
+                + sep.height)
+            canvas:
+                Rectangle:
+                    pos: self.pos
+                    size: self.size
+                    #source: '{}dialog_in_fade.png'.format(images_path)
+                    source: '{}transparent.png'.format(images_path)
             BoxLayout:
-                id: container
-                orientation: 'vertical'
-                size_hint_y: None
-                height: self.minimum_height
-
+                orientation: "vertical"
+                size: root.width, root.height
                 Carousel:
-                    size_hint_y: None
+                    id: carousel
                     loop: True
-                    direction: 'right'
-                    do_scroll_y: False
+                    on_touch_down: (carousel.load_next())
                     FitImage:
                         source: "image1%s.png"%(root.SpotName+root.ID)
-                        radius: [10, ]
+                        size_hint_y: 1
+                        radius: [10,]
                     FitImage:
                         source: "image2%s.png"%(root.SpotName+root.ID)
-                        radius: [10, ]
+                        size_hint_y: 1
+                        radius: [10,]
                     FitImage:
                         source: "image3%s.png"%(root.SpotName+root.ID)
-                        radius: [10, ]
-
+                        size_hint_y: 1
+                        radius: [10,]
                 MDList:
                     id: list_layout
                     height: self.minimum_height
@@ -205,8 +175,6 @@ Builder.load_string(
                             size: self.size
                         Color:
                             rgba: [1,0,0,.5]
-
-
                     ThinBox:
                         ThinLabel:
                             text: "Spot Name: "
@@ -291,19 +259,14 @@ Builder.load_string(
                             text: "Last updated: "
                         ThinLabel:
                             text: root.updateTime
-
         MDSeparator:
             id: sep
-
-
 <ContentMDDialog>
     orientation: 'vertical'
     padding: dp(15)
     spacing: dp(10)
-
     text_button_ok: ''
     text_button_cancel: ''
-
     MDLabel:
         id: title
         text: root.title
@@ -313,21 +276,18 @@ Builder.load_string(
         size_hint_y: None
         text_size: self.width, None
         height: self.texture_size[1]
-
     ScrollView:
         id: scroll
         size_hint_y: None
         height:
             root.height - (box_buttons.height + title.height + dp(48)\
             + sep.height)
-
         canvas:
             Rectangle:
                 pos: self.pos
                 size: self.size
                 #source: f'{images_path}dialog_in_fade.png'
                 source: f'{images_path}transparent.png'
-
         MDLabel:
             text: '\\n' + root.text + '\\n'
             size_hint_y: None
@@ -335,10 +295,8 @@ Builder.load_string(
             valign: 'top'
             halign: 'left' if not root.device_ios else 'center'
             markup: True
-
     MDSeparator:
         id: sep
-
     BoxLayout:
         id: box_buttons
         size_hint_y: None
