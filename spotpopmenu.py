9f86c0faa5acb37dddcc5672da79507bdedf8327
Infinitel8p <bigludovico@aol.com>
2021-08-27 21:45:30 +0200

    update carousel images when loading without inet

--- spotpopmenu.py
+++ spotpopmenu.py
@@ -1,6 +1,7 @@
 from kivymd.uix.dialog import ListMDDialog
 import requests
 import certifi
+import shutil
 
 
 class LocationPopupMenu(ListMDDialog):
@@ -15,35 +16,49 @@ class LocationPopupMenu(ListMDDialog):
             attribute_value = str(spot_data[i])
             setattr(self, attribute_name, attribute_value)
         try:
-            response = requests.get(str(self.Image1), verify=certifi.where())
-            file = open(f"image1{self.SpotName}{self.ID}.png", "wb")
-            file.write(response.content)
-            file.close()
+            if self.Image1 != "":
+                response = requests.get(
+                    str(self.Image1), verify=certifi.where())
+                file = open(f"image1{self.SpotName}{self.ID}.png", "wb")
+                file.write(response.content)
+                file.close()
+            else:
+                response = requests.get(
+                    "https://raw.githubusercontent.com/infinitel8p/Topspot/master/Images/missing.jpg", verify=certifi.where())
+                file = open(f"image1{self.SpotName}{self.ID}.png", "wb")
+                file.write(response.content)
+                file.close()
         except:
-            response = requests.get(
-                "https://raw.githubusercontent.com/infinitel8p/Topspot/master/Images/missing.jpg", verify=certifi.where())
-            file = open(f"image1{self.SpotName}{self.ID}.png", "wb")
-            file.write(response.content)
-            file.close()
+            shutil.copy("error.png", f"image1{self.SpotName}{self.ID}.png")
+
         try:
-            response = requests.get(str(self.Image2), verify=certifi.where())
-            file = open(f"image2{self.SpotName}{self.ID}.png", "wb")
-            file.write(response.content)
-            file.close()
+            if self.Image2 != "":
+                response = requests.get(
+                    str(self.Image2), verify=certifi.where())
+                file = open(f"image2{self.SpotName}{self.ID}.png", "wb")
+                file.write(response.content)
+                file.close()
+            else:
+                response = requests.get(
+                    "https://raw.githubusercontent.com/infinitel8p/Topspot/master/Images/missing.jpg", verify=certifi.where())
+                file = open(f"image2{self.SpotName}{self.ID}.png", "wb")
+                file.write(response.content)
+                file.close()
         except:
-            response = requests.get(
-                "https://raw.githubusercontent.com/infinitel8p/Topspot/master/Images/missing.jpg", verify=certifi.where())
-            file = open(f"image2{self.SpotName}{self.ID}.png", "wb")
-            file.write(response.content)
-            file.close()
+            shutil.copy("error.png", f"image2{self.SpotName}{self.ID}.png")
+
         try:
-            response = requests.get(str(self.Image3), verify=certifi.where())
-            file = open(f"image3{self.SpotName}{self.ID}.png", "wb")
-            file.write(response.content)
-            file.close()
+            if self.Image3 != "":
+                response = requests.get(
+                    str(self.Image3), verify=certifi.where())
+                file = open(f"image3{self.SpotName}{self.ID}.png", "wb")
+                file.write(response.content)
+                file.close()
+            else:
+                response = requests.get(
+                    "https://raw.githubusercontent.com/infinitel8p/Topspot/master/Images/missing.jpg", verify=certifi.where())
+                file = open(f"image3{self.SpotName}{self.ID}.png", "wb")
+                file.write(response.content)
+                file.close()
         except:
-            response = requests.get(
-                "https://raw.githubusercontent.com/infinitel8p/Topspot/master/Images/missing.jpg", verify=certifi.where())
-            file = open(f"image3{self.SpotName}{self.ID}.png", "wb")
-            file.write(response.content)
-            file.close()
+            shutil.copy("error.png", f"image3{self.SpotName}{self.ID}.png")
